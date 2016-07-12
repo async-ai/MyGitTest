dde835a6a99418f15920c1821ad23f6c455ef2d6
async-ai <ws4511@163.com>
2016-07-12 11:46:30 +0800

    无标题提交

--- app/src/main/java/com/demo/jiangyuehua/mygittest/MainActivity.java
+++ app/src/main/java/com/demo/jiangyuehua/mygittest/MainActivity.java
@@ -2,6 +2,7 @@ package com.demo.jiangyuehua.mygittest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
 	}
 }
